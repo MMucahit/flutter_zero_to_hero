d0bfe170aa0a5b26759f3ec5391f7cd4c8bd445d
Muhammed Mücahit NAS <MMucahitNas@gmail.com>
2022-04-14 22:57:58 +0300

    firstCommit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App!',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Material App Bar!'),
+        ),
+        body: Center(
+          child: Text('Center Point!'),
+        ),
+      ),
+    );
+  }
+}
