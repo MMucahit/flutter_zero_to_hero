6e1771f054ce0dbeb1ad9bbb86089b3b5f7e8a91
Muhammed Mücahit NAS <MMucahitNas@gmail.com>
2022-04-15 17:31:10 +0300

    text_learn

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_zero_to_hero/101/text_learn.dart';
 
 void main(List<String> args) {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App!',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar!'),
-        ),
-        body: Center(
-          child: Text('Center Point!'),
-        ),
-      ),
+      theme: ThemeData.light(),
+      home: TextLearnView(),
     );
   }
 }
